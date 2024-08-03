818b2cfe22b82c0a33ee36ece91145659b5447bf
LongDorat <longdorat@outlook.com>
2024-08-03 11:22:52 +0700

    Fix the order of the algorithm

--- [00001] Two Sum/C#/Program.cs
+++ [00001] Two Sum/C#/Program.cs
@@ -9,19 +9,14 @@ int[] TwoSum(int[] nums, int target)
     Dictionary<int, int> dictionary = new Dictionary<int, int>();
     for (int i = 0; i < nums.Length; i++)
     {
-        //1. nums_1 + nums_2 = target => nums_2 = target - nums_1 <=> 2 + 7 = 9 => 7 = 9 - 2
-        if (dictionary.ContainsKey(nums[i])) //2. This line determine if the nums_1 = 2 is in the dictionary or not
+        if (dictionary.ContainsKey(target - nums[i]))
         {
-            return [dictionary[nums[i]], i];//5. Finally we extract the position of nums_1 = 7 and nums_2 = 2
+            return [dictionary[target - nums[i]], i];
         }
-        else if (!dictionary.ContainsKey(target - nums[i]))//? Just a false safe so it not gonna duplicate.
+        else if (!dictionary.ContainsKey(nums[i]))
         {
-            dictionary.Add(target - nums[i], i);  //3. If not we insert nums_2 = target - nums_1 = 9 - 2 = 7 into the dictionary.
+            dictionary.Add(nums[i], i);
         }
-        /*4. After a loop, the position of nums_1 and nums_2 basically swapped.
-            2 + 7 = 9 (2 is nums_1 and 7 is nums_2)
-            After the loop, nums_1 = 7 and nums_2 = 2
-        It is because now we want to check if nums_1 is in the dictionary which already exists.*/
     }
     throw new Exception("No two sum solution");
 }
